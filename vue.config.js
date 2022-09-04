f065c561262c22a80f457ede4d58a5a09389a484
= <1317964417@qq.com>
2022-09-04 18:51:01 +0800

    v1.4

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@views'
+            }
+        }
+    }
 })
+
+
+// module.exports = {
+
+//     configureWebpack: {
+//         resolve: {
+//             alias: {
+//                 'assets': '@/assets',
+//                 'common': '@/common',
+//                 'components': '@/components',
+//                 'network': '@/network',
+//             }
+//         }
+//     }
+// }
+
